e24f092608110985873c64e83709f8593424cdc0
Nandhinisn12 <109675284+Nandhinisn12@users.noreply.github.com>
2022-07-22 16:55:42 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'ANGULAR ' + VERSION.major;
+  public headers = ['Flavour', 'Ratings', 'Price'];
+  public rows = [
+    {
+      Flavour: 'Strawberry',
+      Ratings: '4STAR',
+      Price: '250',
+    },
+    {
+      Flavour: 'Toffee',
+      Ratings: '3STAR',
+      Price: '190',
+    },
+  
+  ];
 }
